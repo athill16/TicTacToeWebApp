d09c6a808b692890b2b1d23b627d4a3e44c57a35
Aaron Hill <aaronthill16@gmail.com>
2016-04-30 15:06:09 -0400

    Displays correct markers for player one and two.

--- app.rb
+++ app.rb
@@ -7,6 +7,19 @@ end
 post '/input' do
 	player_one = params[:player_one]
 	player_two = params[:player_two]
-	erb :player_one_marker,:locals => {:player_one => player_one, :player_two => player_two}
+	erb :player_one_marker, :locals => {:player_one => player_one, :player_two => player_two}
 end
+
+post '/playermarker' do
+	choice_for_x = params[:choice_for_x]
+	if choice_for_x == "Player one"
+		player_one_marker = "X"
+		player_two_marker = "O"
+	else
+		player_one_marker = "O"
+		player_two_marker = "X"
+	end
+	erb :play_game, :locals => {:player_one_marker => player_one_marker, :player_two_marker => player_two_marker}
+end
+
 
