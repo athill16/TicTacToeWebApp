286a1e964271838e89fe78355ed99c6c2b89c8f7
Aaron Hill <aaronthill16@gmail.com>
2016-05-14 19:35:29 -0400

    Human vs Human works

--- app.rb
+++ app.rb
@@ -1,25 +1,81 @@
 require 'sinatra'
+require_relative '../Tic-Tac-Toe-Backend/TTT_AI_Game.rb'
+enable :sessions
 
 get '/' do
 	erb :choose_players
 end
 
 post '/input' do
-	player_one = params[:player_one]
-	player_two = params[:player_two]
-	erb :player_one_marker, :locals => {:player_one => player_one, :player_two => player_two}
+	session[:player_one] = params[:player_one]
+	session[:player_two] = params[:player_two]
+	erb :player_one_marker, :locals => {:player_one => session[:player_one], :player_two => session[:player_two]}
 end
 
 post '/playermarker' do
 	choice_for_x = params[:choice_for_x]
-	if choice_for_x == "Player one"
-		player_one_marker = "X"
-		player_two_marker = "O"
+	if choice_for_x == "player_one"
+		session[:player_one_marker] = "X"
+		session[:player_two_marker] = "O"
 	else
-		player_one_marker = "O"
-		player_two_marker = "X"
+		session[:player_one_marker] = "O"
+		session[:player_two_marker] = "X"
 	end
-	erb :play_game, :locals => {:player_one_marker => player_one_marker, :player_two_marker => player_two_marker}
+	if session[:player_one] != "Human" && session[:player_two] != "Human"
+		if session[:player_one] == "Simple AI"
+			player1 = {:player_mode => Simple.new, :marker => session[:player_one_marker]}
+		else
+			player1 = {:player_mode => Sequential.new, :marker => session[:player_one_marker]}
+		end
+		if session[:player_two] == "Simple AI"
+			player2 = {:player_mode => Simple.new, :marker => session[:player_two_marker]}
+		else 
+			player2 = {:player_mode => Sequential.new, :marker => session[:player_two_marker]}
+		end
+		board = create_new_game_board
+		board = play_game(player1, player2, board)
+		erb :ai_game, :locals => {:player_one_marker => session[:player_one_marker], :player_two_marker => session[:player_two_marker], :player_one => session[:player_one], :player_two => session[:player_two], :board => board}
+	elsif session[:player_one] == "Human" && session[:player_two] == "Human"
+		session[:board] = create_new_game_board
+		erb :human, :locals => {:board => session[:board]}
+	end
 end
+
+post '/humanchoice' do
+	array = []
+	session[:board].each do |element|
+		if element.is_a?(Integer)
+			array << element
+		end
+	end
+	array = array.count
+	if array % 2 != 0
+		space_chosen = params[:choice]
+		space_chosen = space_chosen.to_i
+		board = update_game_board(session[:board], space_chosen-1, session[:player_one_marker])
+		if has_game_been_won?(session[:board], session[:player_one_marker]) == true
+			erb :player_one_human_wins, :locals => {:board => session[:board]}
+		elsif has_game_been_tied?(session[:board]) == true
+			erb :tie_game
+		else
+			erb :human, :locals => {:board => board}
+		end
+	elsif array % 2 == 0
+		space_chosen = params[:choice]
+		space_chosen = space_chosen.to_i
+		board = update_game_board(session[:board], space_chosen-1, session[:player_two_marker])
+		if has_game_been_won?(session[:board], session[:player_two_marker]) == true
+			erb :player_two_human_wins, :locals => {:board => session[:board]}
+		elsif has_game_been_tied?(session[:board]) == true
+			erb :tie_game
+		else
+			erb :human, :locals => {:board => board}
+		end
+	end
+end
+
+
+
+
 
 
